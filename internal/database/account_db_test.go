04e665cde581cb724771f2f7c851de1f7eafb8b0
Robson Silva <robsantossilva@gmail.com>
2022-12-21 22:13:03 -0300

    feat: :sparkles: Account and Client DB

---
+++ internal/database/account_db_test.go
@@ -0,0 +1,64 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/robsantossilva/fullcycle-arquitetura-microsservicos/internal/entity"
+	"github.com/stretchr/testify/suite"
+)
+
+type AccountDBTestSuite struct {
+	suite.Suite
+	db        *sql.DB
+	accountDB *AccountDB
+	client    *entity.Client
+}
+
+func (s *AccountDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+	db.Exec("CREATE TABLE clients (id VARCHAR(255), name VARCHAR(255), email VARCHAR(255), created_at date)")
+	db.Exec("CREATE TABLE accounts (id VARCHAR(255), client_id VARCHAR(255), balance FLOAT, created_at date)")
+	s.accountDB = NewAccountDB(db)
+	s.client, _ = entity.NewClient("Robson", "r@r.com")
+}
+
+func (s *AccountDBTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+	s.db.Exec("DROP TABLE accounts")
+}
+
+func TestAccountDBTestSuite(t *testing.T) {
+	suite.Run(t, new(AccountDBTestSuite))
+}
+
+func (s *AccountDBTestSuite) TestSave() {
+	account := entity.NewAccount(s.client)
+	err := s.accountDB.Save(account)
+	s.Nil(err)
+}
+
+func (s *AccountDBTestSuite) TestGet() {
+
+	s.db.Exec("INSERT INTO clients (id, name, email, created_at) VALUES (?, ?, ?, ?)",
+		s.client.ID, s.client.Name, s.client.Email, s.client.CreatedAt,
+	)
+
+	account := entity.NewAccount(s.client)
+	err := s.accountDB.Save(account)
+	s.Nil(err)
+
+	accountDB, err := s.accountDB.Get(account.ID)
+
+	s.Nil(err)
+	s.Equal(account.ID, accountDB.ID)
+	s.Equal(account.Client.ID, accountDB.Client.ID)
+	s.Equal(account.Balance, accountDB.Balance)
+	s.Equal(account.Client.ID, accountDB.Client.ID)
+	s.Equal(account.Client.Name, accountDB.Client.Name)
+	s.Equal(account.Client.Email, accountDB.Client.Email)
+}
